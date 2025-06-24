33f016d4a58998bfcdf0c38b8a06f41ea0134c46
RafanHaizar <148255036+RafanHaizar@users.noreply.github.com>
2025-06-24 21:12:27 +0600

    Create 124A.cpp

---
+++ 124A.cpp
@@ -0,0 +1,23 @@
+/**
+ * @tagline Bismillahi Allahuakbar, JOY BANGLA!!!
+ * @author Rafan
+ * OIA IOAAA OIIIII
+ *             - Cat
+ */
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+string output_header = " ----OUTPUT----- ";
+string spp = " ";
+string yes = "YES";
+string no = "NO";
+vector<int> dp;
+ 
+ 
+int main() {
+    int a,b,c;
+    cin >> a >> b>> c;
+    cout << min(a-b,c+1) << flush;
+    return 0;
+}
