7111ddaea17b7d4fa3e8b3faccfdf88a8c972523
RafanHaizar <148255036+RafanHaizar@users.noreply.github.com>
2025-06-24 19:19:30 +0600

    Create 2095H.cpp

---
+++ 2095H.cpp
@@ -0,0 +1,56 @@
+/**
+ * @tagline Bismillahi Allahuakbar, JOY BANGLA!!!
+ * @author Rafan
+ * OIA IOAAA OIIIII
+ *             - Cat
+ */
+ 
+#include <bits/stdc++.h>
+ 
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef double dbl;
+typedef string str;
+ 
+str output_header = " ----OUTPUT----- ";
+str spp = " ";
+str yes = "YES";
+str no = "NO";
+vector<long long int> dp;
+ 
+#define opp_endl(condition) ((condition) ? (cout << yes << endl << flush) : (cout << no << endl<<flush))
+#define opp(condition) ((condition) ? (cout << yes << flush) : (cout << no << flush))
+ 
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    if(n==1){
+        cout << "CODEFORCES" <<flush;
+    }else if(n==2){
+        cout << "EYE" <<flush;
+    }else if(n==3){
+        cout << "TESTING" <<flush;
+    }else if(n==4){
+        cout << "SYSTEM" <<flush;
+    }else if(n==5){
+        cout << "APRIL" <<flush;
+    }else if(n==6){
+        cout << "FOOLS" <<flush;
+    }else if(n==7){
+        cout << "YOU" <<flush;
+    }else if(n==8){
+        cout << "READ" <<flush;
+    }else if(n==9){
+        cout << "POORLY" <<flush;
+    }else if(n==10){
+        cout << "GET" <<flush;
+    }else if(n==11){
+        cout << "EYEGLASSES" <<flush;
+    }
+    return 0;
+}
