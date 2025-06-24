5cecb3a0b55abf4626cd21767a452e4917f3e037
RafanHaizar <148255036+RafanHaizar@users.noreply.github.com>
2025-06-24 21:09:20 +0600

    Create 1505A.cpp

---
+++ 1505A.cpp
@@ -0,0 +1,23 @@
+/**
+ * @tagline Bismillahi Allahuakbar, JOY BANGLA!!!
+ * @author Rafan
+ * OIA IOAAA OIIIII
+ *             - Cat
+ */
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+string output_header = " ----OUTPUT----- ";
+string spp = " ";
+string yes = "YES";
+string no = "NO";
+vector<long long int> dp;
+ 
+int main() {
+    string v;
+    while(getline(cin,v)){
+        cout << no << endl << flush;
+    }
+    return 0;
+}
